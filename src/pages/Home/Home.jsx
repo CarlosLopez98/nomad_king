0a83fdd42b64a65ce0cb78f49b69730188997c79
carlos <carlos980718@gmail.com>
2024-05-24 19:46:29 -0500

    [ADD] Ilustraciones y textos para la primera version de la landing page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import './Home.css';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
-import Avatar from '../../components/Avatar'
-import { Link } from 'react-router-dom'
+import Avatar from '../../components/Avatar';
+import { Link } from 'react-router-dom';
 
-import avatars from '../../data/avatars'
+import avatars from '../../data/avatars';
+
+// Illustrations
+import searchIl from '../../assets/images/searching.svg';
+import meetingIl from '../../assets/images/meeting.svg';
+import coupleIl from '../../assets/images/couple.svg';
+import travelIl from '../../assets/images/travel.svg';
 
 const Home = () => {
   return (
@@ -41,12 +47,29 @@ const Home = () => {
         <section className="info">
           <div className="info-1">
             <h3>Find places around the city</h3>
-            <div className="ilustration"></div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris a mollis  ex. Vivamus in porta turpis, a bibendum libero. Duis maximus ac purus  vitae egestas. Donec rhoncus placerat neque, eget blandit lacus bibendum  eget.</p>
+            <div className="ilustration">
+              <div className="box"></div>
+              <img src={searchIl} alt="Girl searching in the platform." width={800} />
+            </div>
+            <p>Find the perfect work spot in any city! Our platform helps remote workers discover efficient, convenient locations to stay productive. Explore top-rated cafes, coworking spaces, and more tailored to your needs.</p>
           </div>
 
           <div className="info-2">
             <h3>Arrive and work without problems</h3>
+            <div className="ilustration">
+              <div className="circle"></div>
+              <img src={meetingIl} alt="Girls having a meeting." width={800} />
+              <p>Arrive in any city and start working seamlessly! Our platform connects remote workers with ideal locations for productivity. Discover cafes, coworking spaces, and more designed for your remote work needs.</p>
+            </div>
+          </div>
+
+          <div className="info-3">
+            <div className="ilustration">
+              <h3 className="text-meet">Meet new<br />people</h3>
+              <h3 className="text-travel"><span>Keep</span><br /> traveling</h3>
+              <img src={coupleIl} alt="A couple resting in their backs." width={800} className="couple" />
+              <img src={travelIl} alt="A couple sitting in a motorcicle." width={800} className="travel" />
+            </div>
           </div>
         </section>
       </main>
