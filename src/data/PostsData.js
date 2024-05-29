c45046d15f9e2bf3df2a505a9cabafabeea724c7
carlos <carlos980718@gmail.com>
2024-05-29 17:32:11 -0500

    [FIX] Correcion en el scroll

--- src/data/PostsData.js
+++ src/data/PostsData.js
@@ -1,28 +1,28 @@
 import postPic1 from '../assets/images/postpic1.jpg'
 import postPic2 from '../assets/images/postpic2.jpg'
-import postPic3 from '../assets/images/postpic3.JPG'
+import postPic3 from '../assets/images/postpic3.jpg'
 
 
 export const postsData = [
     {
         img: postPic1,
         name: 'Tzuyu',
-        desc: "Happy New Year all friends! #2023",
+        desc: "Meeting with coworkers!",
         likes: 2300,
         liked: true
     },
     {
         img: postPic2,
         name: 'Maryam',
-        desc: "Party time :)",
+        desc: "What a place, great fly.",
         likes: 2300,
         liked: false
 
     },
     {
         img:postPic3,
-        name: "Salena Gomez",
-        desc: "At Archery Festival",
+        name: "Selena Gomez",
+        desc: "This city is amazing, i'm loving it.",
         likes: 800,
         liked: false
     }
